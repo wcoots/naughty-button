7de9c95b84e94e7f98b5b0f7a8581c9cde9caa06
WillCooter <will@cooters.co.uk>
2020-11-03 17:25:06 +0000

    composition api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
         @mouseover="mouseOver"
         @mouseleave="mouseLeave"
         @click="click">
-          Click me
+          {{ button_text }}
       </button>
   </span>
   <span v-else>
@@ -15,62 +15,17 @@
 
 </template>
 
-<script>
+<script lang="ts">
+import { declareInitialValues, getPageDimensions, computedPosition, mouseOver, mouseLeave, click } from '../hooks'
+
 export default {
   name: 'Home',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      clicked: false,
-      mouse_over: false,
-      left: 100,
-      top: 100,
-      max_height: 0,
-      max_width: 0,
-    }
-  },
-  created() {
-    this.max_width = Math.max(
-      document.body.scrollWidth,
-      document.documentElement.scrollWidth,
-      document.body.offsetWidth,
-      document.documentElement.offsetWidth,
-      document.documentElement.clientWidth
-    ) - 50
+  setup() {
+    const { clicked, button_text } = declareInitialValues()
+    getPageDimensions()
 
-    this.max_height = Math.max(
-      document.body.scrollHeight,
-      document.documentElement.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.offsetHeight,
-      document.documentElement.clientHeight
-    ) - 50
+    return { clicked, button_text, computedPosition, mouseOver, mouseLeave, click }
   },
-  computed: {
-    computedPosition() {
-      return {
-        left: this.left,
-        top: this.top,
-      }
-    }
-  },
-  methods: {
-    mouseOver() {
-      this.mouse_over = true
-      this.left = `${Math.round(Math.random() * this.max_width)}px`
-      this.top = `${Math.round(Math.random() * this.max_height)}px`
-    },
-    click() {
-      if (this.mouse_over) {
-        this.clicked = true
-      }
-    },
-    mouseLeave() {
-      this.mouse_over = false
-    },
-  }
 }
 </script>
 
@@ -81,10 +36,13 @@ export default {
     float: left;
     display: block;
     position: absolute;
-    background-color: #4e9251;
+    font-weight: bold;
+    color: #082514;
+    background-color: #7a9b7c;
     border-left: solid #186438 2px;
     border-right: solid #186438 2px;
     border-bottom: solid #186438 2px;
+    border-radius: 25px;
     margin-bottom: 50px;
     padding-top: 10px;
     padding-bottom: 10px;
